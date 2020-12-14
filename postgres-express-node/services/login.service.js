d0c6eca52e97d61e9d8c2db7da88c20795fae8da
Neelifa <antonela.stanic11@gmail.com>
2020-12-14 12:13:45 +0100

    Dodana sigurna provjera lozinki

--- postgres-express-node/services/login.service.js
+++ postgres-express-node/services/login.service.js
@@ -1,3 +1,4 @@
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const config = require("../config");
 
@@ -18,8 +19,9 @@ class LoginService {
     }
 
     this.logger.info("Checking password...");
+    const validPassword = await bcrypt.compare(password, userRecord.password);
     
-    if(userRecord.password === password) {      
+    if(validPassword) {      
       this.logger.info("Password correct, proceed and generate JWT!");
       
       const user = {
